14c9a7c71cd26afe73a31bdb40528fe32b52035b
Jonathan <jonathann.maimon@gmail.com>
2018-02-04 13:57:45 +0200

    Added routers list

---
+++ src/components/RoutersList.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import App from './App';
+import LoginPage from './pages/LoginPage';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+class RoutersList extends Component {
+    render() {
+        return (
+        <Router>
+            <Switch>
+                <Route path="/" exact component={App} />
+                <Route render={() => <h1>Page not found</h1>} />
+            </Switch>
+        </Router>
+        )
+    }
+}
+
+export default RoutersList;
