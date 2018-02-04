39338a5177b08836c8e18f032a954c8b6f079809
Zvukamar <fre3run@gmail.com>
2018-02-04 20:22:13 +0200

    added modal - need to fix

--- src/components/forms/signin.js
+++ src/components/forms/signin.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Form, FormGroup, Button, ControlLabel, FormControl } from 'react-bootstrap';
+import { Form, FormGroup, Button, ControlLabel, FormControl, Modal } from 'react-bootstrap';
 import axios from 'axios';
 
 const url = 'https://keepers-server-develop-features.eu-gb.mybluemix.net/keeper-server/users/login';
@@ -13,7 +13,8 @@ class SignInForm extends React.Component {
     }
     render() {
         return(
-            <Form onSubmit={this.signIn.bind(this)}>
+            <div>
+                 <Form onSubmit={this.signIn.bind(this)}>
                 <FormGroup controlId="formInlineEmail">
                     <ControlLabel>Email</ControlLabel>{' '}
                     <FormControl type="email" placeholder="Email" inputRef={ref => this.email = ref} />
@@ -24,6 +25,26 @@ class SignInForm extends React.Component {
                     <Button type="submit">Sign In</Button>
                 </FormGroup>
             </Form>
+            <Modal
+            show={true}
+            onHide={this.handleHide}
+            container={this}
+            aria-labelledby="contained-modal-title"
+          >
+            <Modal.Header closeButton>
+              <Modal.Title id="contained-modal-title">
+                Contained Modal
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              Elit est explicabo ipsum eaque dolorem blanditiis doloribus sed id
+              ipsam, beatae, rem fuga id earum? Inventore et facilis obcaecati.
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={this.handleHide}>Close</Button>
+            </Modal.Footer>
+          </Modal>
+            </div>
         );
     }
 
@@ -39,7 +60,9 @@ class SignInForm extends React.Component {
         })
         .then(res => console.log(res))  // todo:redirect
         .catch(error => {
-            if(error.response.data.message === 'Password does not match') {
+            if(error.response.data.message === 'Email does not exists') {
+
+            } else if(error.response.data.message === 'Password does not match') {
                 this.setState({
                     validPassword: "error"
                 });
