32ed1ec4957ff8832633e60cf2699b7ce82578b6
Zvukamar <fre3run@gmail.com>
2018-02-04 19:18:34 +0200

    added signin form and logic

--- src/components/forms/signin.js
+++ src/components/forms/signin.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Form, FormGroup, Button, ControlLabel, FormControl } from 'react-bootstrap';
+import axios from 'axios';
 
 class SignInForm extends React.Component {
     render() {
@@ -7,11 +8,11 @@ class SignInForm extends React.Component {
             <Form inline onSubmit={this.signIn.bind(this)}>
                 <FormGroup controlId="formInlineEmail">
                     <ControlLabel>Email</ControlLabel>{' '}
-                    <FormControl type="email" placeholder="Email" ref="email" />
+                    <FormControl type="email" placeholder="Email" inputRef={ref => this.email = ref} />
                 </FormGroup>{' '}
                 <FormGroup controlId="formInlinePassword">
                     <ControlLabel>Password</ControlLabel>{' '}
-                    <FormControl type="password" placeholder="Password" ref="password"/>{' '}
+                    <FormControl type="password" placeholder="Password" inputRef={ref => this.password = ref}/>{' '}
                     <Button type="submit">Sign In</Button>
                 </FormGroup>
             </Form>
@@ -21,7 +22,9 @@ class SignInForm extends React.Component {
     
     signIn(event) {
         event.preventDefault(); // prevent auto refresh the page after submit.
-        console.log(this.refs);
+        var email = this.email.value;
+        var password = this.password.value;
+        axios.get('http://api.icndb.com/jokes/random').then(res => console.log(res.data.value.joke)).catch(err => console.log(err));
     }
 }
 
