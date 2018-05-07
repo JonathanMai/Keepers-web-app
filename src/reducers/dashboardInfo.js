dd5327484b410cfa1b506650d648ac7f460106e0
Jonathan <jonathann.maimon@gmail.com>
2018-05-07 16:17:36 +0300

    Added redux to dashboard.

---
+++ src/reducers/dashboardInfo.js
@@ -0,0 +1,28 @@
+import moment from 'moment';
+const initialState = {
+    childrens: [],
+    dates: [],
+    datesRange: 0
+};
+
+const dashboardInfo = (state = initialState, action) => {
+    switch(action.type) {
+        case "SET_CHILDRENS":
+            return {
+                ...state,
+                childrens: action.value
+            };
+        case "SET_DATES":
+        console.log(moment.utc(action.value[1]).startOf('day').diff(moment.utc(action.value[0]).startOf('day'), 'days') )
+
+            return {
+                ...state,
+                dates: action.value,
+                datesRange: (action.value.length > 1) ? moment.utc(action.value[1]).startOf('day').diff(moment.utc(action.value[0]).startOf('day'), 'days') : 1
+            };
+        default: 
+            return state;
+    }
+};
+
+export default dashboardInfo;
--- src/store.js
+++ src/store.js
@@ -1,8 +1,9 @@
 import { createStore, combineReducers } from 'redux';
 import reducerA from './reducers/reducerA';
 import reducerAccountInfo from './reducers/reducerAccountInfo';
+import dashboardInfo from './reducers/dashboardInfo';
 
-const store = createStore(combineReducers({reducerA: reducerA, reducerAccountInfo: reducerAccountInfo}));
+const store = createStore(combineReducers({reducerA: reducerA, reducerAccountInfo: reducerAccountInfo, dashboardInfo: dashboardInfo}));
 
 export default store;
 
