f4a51a30e2dc267234f4fb27d29e01b075cd114c
Jonathan <jonathann.maimon@gmail.com>
2018-05-18 16:41:17 +0300

    Fixed an algorithm that saves the data after each api call.

--- src/components/panels/BarChartPanel.js
+++ src/components/panels/BarChartPanel.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import { Bar, Line, Pie } from 'react-chartjs-2';
 import { connect } from 'react-redux';
 import moment from 'moment';
 import { GetUsageStatistics } from '../../serviceAPI';
--- src/components/panels/LineChartPanel.js
+++ src/components/panels/LineChartPanel.js
@@ -10,75 +10,40 @@ export class LineChart extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            data: {}, // {"18-05-18" : {***}, "19-05-18": {***}}
+            // useData: [],
             dateLabels: [],
             tickValues: []
         }
         // this.buildChart = this.buildChart.bind(this);
-        this.getChildMessagesStatistics(this.props);
+        this.getChildMessagesStatisticsByRange(this.props);
     }
     
     componentWillReceiveProps(props){
-        // console.log()
-        this.getChildMessagesStatistics(props);
-        
+        // this.setState({
+        //     ...this.state,
+        //     useData: []
+        // });
+        this.getChildMessagesStatisticsByRange(props);
     }
-    componentDidCatch() {
-        // this.getLabels();
-    //   this.getChildMessagesStatistics();
-    }
-
-    //     getLabels() {
-    //     let day = moment.utc(this.props.dates[0]); // Creates a moment object from the first day.
-    //     let labels = [];
-    //     let tickVals = [];
-    //     if(this.props.range <= 1) {
-    //         for(let i=0; i<=24; i++) {
-    //             let format = 'HH:MM';
-    //             if(i == 0 || i == 24)
-    //                 format = 'MMM Do';
-    //             labels.push(moment.utc(day).add(i,'hours').format(format).toString());
-    //             if(i%3 === 0) {
-    //                 tickVals.push(labels[i]);
-    //             }
-    //         }
-    //     }
-    //     else {
-    //         for(let i=0; i<=this.props.range; i++) {
-    //             labels.push(moment.utc(day).add(i,'days').format("MMM Do").toString());
-    //             if(this.props.range > 7) {
-    //                 if(i%3 === 0) {
-    //                     tickVals.push(labels[i]);
-    //                 }
-    //             } 
-    //             else {
-    //                 tickVals.push(labels[i]);
-    //             }
-    //         }            
-    //     }
-
-    //     this.setState({
-    //       ...this.state,
-    //       dateLabels: labels,
-    //       tickValues: tickVals
-    //     });
-
-
-    // }
 
     // Gets the child statistics and sets data using it.
-    getChildMessagesStatistics(props) {
+    getChildMessagesStatisticsByRange(props) {
+        // console.log(props);
         // console.log(props.dates[0],props.dates[1],props.dates[0].isSame(props.dates[1]));
         let day = moment(props.startDate); // Creates a moment object from the first day.
-        console.log(props);
-        let newData = [];
+        // let newData = [];
         let range = props.isOneDay ? 24 : props.range;
         let addBy = props.isOneDay ? 'hours' : 'day';
         let tempDay =  moment(day);
         let tickVals = new Array((range > 7) ? range/5 | 0 : range);
-        let countEasy = new Array(range);
-        let countMedium = new Array(range);
-        let countHard = new Array(range);
+        let countEasy = new Array(range+1);
+        let countMedium = new Array(range+1);
+        let countHard = new Array(range+1);
+        let newData = this.state.data;
         let flag = 0;
+        // let insertToData = false;
+        // console.log(this.state);
         // console.log((range > 7 || range <= 1)? "YEAHH" : "NOOOOOOO");
         for(let i=0; i<=range;  i++, tempDay=moment(day).add(i,addBy)) {
             // datesLabel.push(moment(day).format("MMMM DD"));
@@ -87,79 +52,121 @@ export class LineChart extends Component {
             //     console.log(child, moment(tempDay).startOf('day').valueOf(),"and", moment(tempDay).endOf('day').valueOf())
             //     console.log(child, moment.utc(tempDay).startOf('day').valueOf(),"and", moment.utc(tempDay).add(1,'days').startOf('day').valueOf())
             // }
-            let startTime;
-            let endTime;
-            if(props.isOneDay) {
-                startTime = moment(tempDay);
-                endTime = moment(tempDay).add(1, 'hours');
-            } 
-            else {
-                startTime = moment(tempDay).startOf('day');
-                endTime =  moment(tempDay).endOf('day');
-            }
-            // console.log(tempDay);
-            let label =  "";
-            if(props.isOneDay)
-                if(i === 0 || i === range)
-                    label += moment(tempDay).format("MMM Do Ha").toString();
-                else
-                    label += moment(tempDay).format("Ha").toString();
-            else
-                label += moment(tempDay).format("MMM Do").toString();
-            
-            if(range > 7 && (i%5 === 0 || i===range)) {
-                // if(i === range && range%8 !== 0)
-                    tickVals[i/5 | 0] = label;
-            }
-            else if(range <= 7)
-                tickVals[i] = label;
-            //  (range <= 1 && i !== ) ? moment(tempDay).add(i, 'hours').valueOf() :.valueOf();
-            // (range <= 1) ? moment(tempDay).add(i+1, 'hours').valueOf() :.valueOf();
-            GetMessagesStatistics(props.childId, startTime, endTime).then(res => {  // When respond package is with status 200
-                let result = res.data;
-
-                countEasy[i] = this.createStatisticObject(label, parseInt(result.easyCount)); // easy count.
-                countMedium[i] = this.createStatisticObject(label, parseInt(result.mediumCount)); // medium count.
-                countHard[i] = this.createStatisticObject(label, parseInt(result.heavyCount)); // heavy count.
+            // Checks if data is available in array.
+            // console.log(newData);
+            // console.log(objectNames);
+            // console.log(!props.isOneDay && this.state.data !== undefined && this.state.data[tempDay.format("YY-MM-DD")] !== undefined);
+            if(!props.isOneDay && this.state.data !== undefined && newData[tempDay.format("YY-MM-DD")] !== undefined) {
+                let format = tempDay.format("YY-MM-DD");
+                this.insertTickVal(tickVals, newData[format][0]["x"], range, i);
+                countEasy[i] = this.state.data[format][0]; // easy count.
+                countMedium[i] = this.state.data[format][1];// medium count.
+                countHard[i] = this.state.data[format][2]; // heavy count.
                 flag++;
-                if(flag > range){
-                    let tempData = [];
-                    tempData.push(countEasy);
-                    tempData.push(countMedium);
-                    tempData.push(countHard);
-                    // this.createStatisticsDataset(tempData);
-                    this.setState({
-                      ...this.state,
-                      data: tempData,
-                      tickValues: tickVals
-                  });
-                    // if(this.state.draw[0] == false){
-                    //     let tempDraw = this.state.draw;
-                    //     tempDraw[0] = true;
-                    //     this.setState({
-                    //         ...this.state,
-                    //         draw: tempDraw
-                    //     });
-                    //     console.log(this.state.draw);
-                    // }
+                if(flag > range) {
+                    this.insertNewData(newData, [countEasy, countMedium, countHard], tickVals, props.isOneDay);
                 }
-            }).catch(error => { // When respond package is with error status - 400 ...
-                console.log(error.data);
-            });
-        };
-        // }
+            }
+            // If data wasnt called before - fetch data from server.
+            else {
+                console.log("IN API");
+                // insertToData = true;
+                let startTime;
+                let endTime;
+                if(props.isOneDay) {
+                    startTime = moment(tempDay);
+                    endTime = moment(tempDay).add(1, 'hours');
+                } 
+                else {
+                    startTime = moment(tempDay).startOf('day');
+                    endTime =  moment(tempDay).endOf('day');
+                }
+                // console.log(tempDay);
+                let label =  "";
+                if(props.isOneDay) {
+                    if(i === 0 || i === range)
+                        label += moment(tempDay).format("MMM Do Ha").toString();
+                    else
+                        label += moment(tempDay).format("Ha").toString();
+                }
+                else
+                    label += moment(tempDay).format("MMM Do").toString();
+                    
+                this.insertTickVal(tickVals, label, range, i);
+                //  (range <= 1 && i !== ) ? moment(tempDay).add(i, 'hours').valueOf() :.valueOf();
+                // (range <= 1) ? moment(tempDay).add(i+1, 'hours').valueOf() :.valueOf();
+                GetMessagesStatistics(props.child.id, startTime, endTime).then(res => {  // When respond package is with status 200
+                    let result = res.data;
+                    countEasy[i] = this.createStatisticObject(label, parseInt(result.easyCount)); // easy count.
+                    countMedium[i] = this.createStatisticObject(label, parseInt(result.mediumCount)); // medium count.
+                    countHard[i] = this.createStatisticObject(label, parseInt(result.heavyCount)); // heavy count.
+                    newData[moment(day).add(i, 'day').format("YY-MM-DD")] = [countEasy[i], countMedium[i], countHard[i]];
+                    flag++;
+                    if(flag > range)
+                        this.insertNewData(newData, [countEasy, countMedium, countHard], tickVals, props.isOneDay);
+
+                // if(this.state.draw[0] == false){
+                //     let tempDraw = this.state.draw;
+                //     tempDraw[0] = true;
+                //     this.setState({
+                //         ...this.state,
+                //         draw: tempDraw
+                //     });
+                //     console.log(this.state.draw);
+                // }
+            
+                }).catch(error => { // When respond package is with error status - 400 ...
+                    console.log(error);
+                });
+            };
+        }
     }
 
-    createStatisticObject(date, data) {
-      return ({
-        "x": date,
-        "y": data
-      });
+    insertNewData(newData, tempData, tickVals, isOneDay) {
+        // let data = this.state.data;
+        // if (insertToData) data.push(newData);
+        // console.log(tempData, newData);
+        this.setState({
+            ...this.state,
+            data: newData,
+            useData: tempData,
+            tickValues: tickVals
+        });
+
+        // let tempData = [];
+        // tempData.push(countEasy);
+        // tempData.push(countMedium);
+        // tempData.push(countHard);
+        // let data = this.state.data;
+        // if(insertToData)
+        //     data.push(newData);
+
+        // this.createStatisticsDataset(tempData);
+            // console.log(this.state);
     }
+
+    insertTickVal(tickVals, label, range, index) {
+        if(range > 7 && (index%5 === 0 || index===range)) {
+            // if(i === range && range%8 !== 0)
+                tickVals[index/5 | 0] = label;
+        }
+        else if(range <= 7)
+            tickVals[index] = label;
+    }
+    getMessagesStatisticsByRange(){}
+
+    createStatisticObject(date, count) {
+        return ({
+            "x": date,
+            "y": count
+        });
+    }
+
 
     render() {
         // this.getChildMessagesStatistics();
-        return (this.state.data !== undefined && <AbusiveConversationsChart data={this.state.data} tickValues={this.state.tickValues} />);
+        // return <div>ok</div>
+        return (this.state.useData !== undefined && <AbusiveConversationsChart data={this.state.useData} tickValues={this.state.tickValues} />);
     }
 }
 
@@ -167,7 +174,7 @@ export class LineChart extends Component {
 
 function LineChartPanel(props) {
     // console.log(props);
-    return <LineChart childId={props.childrens[props.childIndex].id} startDate={props.startDate} range={props.range} isOneDay={props.isOneDay} />
+    return <LineChart child={props.childrens[props.childIndex]} startDate={props.startDate} range={props.range} isOneDay={props.isOneDay} />
 }
 
 const mapStateToProps = (state) => {
