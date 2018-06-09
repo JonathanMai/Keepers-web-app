8ab0502ba99344a6887753fe6f05d6b4626e2958
Alex Havkin <fre3run@gmail.com>
2018-06-09 13:45:48 +0300

    fixed some little bugs

--- src/components/RoutersList.js
+++ src/components/RoutersList.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import App from './App';
-// import LoginPage from './pages/LoginPage';
-// import RestartPasswordPage from './pages/RestartPasswordPage';
+import LoginPage from './pages/LoginPage';
+import RestartPasswordPage from './pages/RestartPasswordPage';
 import Dashboard from './pages/Dashboard';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
@@ -11,9 +11,9 @@ class RoutersList extends Component {
         <Router>
             <Switch>
                 <Route path="/" exact component={App} />
-                {/* <Route path="/login" exact component={LoginPage} />
+                <Route path="/login" exact component={LoginPage} />
                 <Route path="/register" exact component={LoginPage} />
-                <Route path="/restore-password" exact component={RestartPasswordPage} /> */}
+                <Route path="/restore-password" exact component={RestartPasswordPage} />
                 <Route path="/keepers-dashboard" exact component={Dashboard} />
                 <Route render={() => <h1>Page not found</h1>} />
             </Switch>
