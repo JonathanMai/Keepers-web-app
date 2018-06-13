70104ed9c5d49af32bd3916f2371329748f4859c
Alex Havkin <fre3run@gmail.com>
2018-06-13 10:19:31 +0300

    fixed modal style

--- src/components/modals/RegisterModal.js
+++ src/components/modals/RegisterModal.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import { Modal, Button, Image } from 'react-bootstrap';
+import { Modal, Image } from 'react-bootstrap';
 import PropTypes from 'prop-types';
 import wave from '../../assets/wave.png';
 import arrow from '../../assets/arrow.png';
 import '../../styles/modal.css';
 
-// PROPS.REGISTER
-// PROPS.CLOSE
 export const RegisterModal = (props) => {
         return (
             <Modal
@@ -15,8 +13,8 @@ export const RegisterModal = (props) => {
                 aria-labelledby="contained-modal-title">
                 <Image className="wave" src={wave} />
                 <div className="modal_info">
-                    <span>We notice that you are a new user, lets create a new account!</span>
-                    <span>Sign Up <Image className="modal_arrow" onClick={props.registerUser} src={arrow} /></span>
+                    <span className="invite">We notice that you are a new user, lets create a new account!</span>
+                    <p className="sign_up" onClick={props.registerUser} >Sign Up <Image className="modal_arrow" src={arrow} /></p>
                 </div>
             </Modal>
         );
