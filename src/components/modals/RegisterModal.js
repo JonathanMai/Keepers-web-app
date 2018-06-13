afca616614eb2360bf870d8d33f0aed55d5ac2c1
Alex Havkin <fre3run@gmail.com>
2018-06-13 10:47:56 +0300

    refactor modal css

--- src/components/modals/RegisterModal.js
+++ src/components/modals/RegisterModal.js
@@ -13,7 +13,7 @@ export const RegisterModal = (props) => {
                 aria-labelledby="contained-modal-title">
                 <Image className="wave" src={wave} />
                 <div className="modal_info">
-                    <span className="invite">We notice that you are a new user, lets create a new account!</span>
+                    <span className="invite">We notice that you are a new user, <br/>lets create a new account!</span>
                     <p className="sign_up" onClick={props.registerUser} >Sign Up <Image className="modal_arrow" src={arrow} /></p>
                 </div>
             </Modal>
