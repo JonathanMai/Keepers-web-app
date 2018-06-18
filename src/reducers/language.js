47e0ada3e20eb3262a550d9ea039458b22ee4cc9
Alex Havkin <fre3run@gmail.com>
2018-06-18 21:47:54 +0300

    added comments on reducers

--- src/reducers/language.js
+++ src/reducers/language.js
@@ -1,11 +1,19 @@
 import languages from '../lang/lang';
+
+/* 
+    This redux reducer save the information about languages of the application.
+    Also it has some redux functions to set the language of the application.
+*/
+
+// Initial state
 const initialState = {
-    lang: languages,
-    currLang: languages.eng
+    lang: languages,    // all the languages objects
+    currLang: languages.eng // the current language object
 };
 
 const language = (state = initialState, action) => {
     switch(action.type) {
+        // set the current language object -> action.value is object {key: value}
         case "SET_LANG":
             return {
                 ...state,
--- src/reducers/reducerA.js
+++ src/reducers/reducerA.js
@@ -1,34 +1,45 @@
+/* 
+    This redux reducer save the information about banner/login/register and password reset screens.
+    Also it has some redux functions to set the screen variables.
+*/
+
+// Initial state
 const initialState = {
-    showModal: false,
-    panel_color: "transparent",
-    agreement: false,
-    showIcon: false,
-    showLoadingModal: false
+    showModal: false,       // register modal - boolean variable can be on or off
+    panel_color: "transparent", // color of the panel - initial in login its transparent and in dashboard it has keepers color
+    agreement: false,       // checkbox of terms - must be true to login
+    showIcon: false,        // boolean variable - false in login page and true in dashboard page.
+    showLoadingModal: false // loading modal - boolean variable can be on or off while loading
 };
 
 const reducerA = (state = initialState, action) => {
     switch(action.type) {
+        // action.value is boolean variable can be true or false, set the register modal to be on or off
         case "SET_SHOW_MODAL":
             return {
                 ...state,
                 showModal: action.value
             };
+        // action.value is string variable of color e.g: red, green, blue, etc...
         case "CHANGE_PANEL_COLOR":
             return {
                 ...state,
                 panel_color: action.value
-            };   
+            };
+        // action.value is boolean variable - tick of checkbox can be true or false if the checkbox is checked or not.   
         case "SET_AGREEMENT":
             return {
                 ...state,
                 agreement: action.value
-            };   
+            };
+        // action.value is boolean variable - set the visibility of the logout icon, in login is false and in dashboard must be true
         case "SET_ICON_VISIBILITY":
             return {
                 ...state,
                 showIcon: action.value,
                 panel_color: "rgba(37, 185, 204, 0.45)"
             }; 
+        // action.value is boolean variable - set the visibility of loading modal - true if we want to show the loading modal.
         case "SET_SHOW_LOADING_MODAL":
             return {
                 ...state,
