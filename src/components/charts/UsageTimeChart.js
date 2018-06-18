7b4f9e64c82ba3e89430a916d1a264123d7088e7
Jonathan <jonathann.maimon@gmail.com>
2018-06-18 19:51:56 +0300

    Comments added

--- src/components/charts/UsageTimeChart.js
+++ src/components/charts/UsageTimeChart.js
@@ -1,26 +1,17 @@
 import React, { Component } from 'react';
-import { Bar, Line, Pie } from 'react-chartjs-2';
+import { Bar } from 'react-chartjs-2';
+
+/*
+    Usage time chart draws a chart of usage data.
+    In the chart the user can see the child usage data - how much time he spent on facebook, WhatsApp etc.
+    The data shown in minuts/hours and shows the application which the child used.
+*/
 
 class UsageTimeChart extends Component {
     constructor(props) {
         super(props);
+        // Bind functions.
         this.getTooltipLabel = this.getTooltipLabel.bind(this);
-    }
-
-    getTooltipLabel(tooltipItem) {
-        let label = "";
-
-        // Data shown in hours.
-        if(this.props.type === "h") {
-            let time = tooltipItem.yLabel * 60;
-            label = Math.ceil(tooltipItem.yLabel) + " Hours and " + Math.ceil(time % 60) + " Minutes"
-        }
-        // Data shown in minutes.
-        else {
-            label += tooltipItem.yLabel + " Minutes"
-        }
-
-        return label;
     }
 
     render() {
@@ -28,6 +19,7 @@ class UsageTimeChart extends Component {
             <div className="chart" style={{position: "relative", height:"100%", width:"inherit"}}>
                 <Bar   
                     ref = "chart"
+                    // Data that will be drawn on the chart.
                     data={{                
                         labels: this.props.labels,
                         datasets: [{
@@ -43,10 +35,8 @@ class UsageTimeChart extends Component {
                         scales: {
                             yAxes: [{
                                 ticks: {
-                                    beginAtZero: true
-                                    // callback: function(value, index, values) {
-                                    //     return '$' + value;
-                                    // }
+                                    beginAtZero: true,
+                                    callback: (value) =>{if(Number.isInteger(value)) return (value+this.props.type)} // Shows only whole numbers.
                                 }
                             }],
                             xAxes: [{
@@ -65,7 +55,7 @@ class UsageTimeChart extends Component {
                             caretSize: 0,
                             callbacks: {
                                 title: function() {},
-                                label: (tooltipItem, data) => this.getTooltipLabel(tooltipItem)
+                                label: (tooltipItem) => this.getTooltipLabel(tooltipItem)
                             }
                         },
                         legend: {
@@ -77,5 +67,22 @@ class UsageTimeChart extends Component {
         );
 
     }
+
+    // Tooltip label - returns the tooltip label content.
+    // Gets the tooltip item and returns the new label.
+    getTooltipLabel(tooltipItem) {
+        let label = "";
+         // Data shown in hours.
+        if(this.props.type === "h") {
+            let time = tooltipItem.yLabel * 60;
+            label = Math.ceil(tooltipItem.yLabel) + " Hours and " + Math.ceil(time % 60) + " Minutes"
+        }
+        // Data shown in minutes.
+        else {
+            label += tooltipItem.yLabel + " Minutes"
+        }
+        return label;
+    }
 }
+
 export default UsageTimeChart;
